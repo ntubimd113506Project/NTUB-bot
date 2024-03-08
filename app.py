f2b07e041ef6345d267cfe030571339ed4254224
10956048 <151896463+10956048@users.noreply.github.com>
2024-03-08 22:12:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ import time
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
-line_bot_api = LineBotApi('XmDgqV5HIEHoZrpt0RBp9xs4bF4VpyjxGficddIuujMzJpp2SCs515e/V0e/FrOTBp2UTXnBof1jhi++LhV+aprOgwzQxJrGzvuyqQxmSWJ9j7sr6Sow7cdsgp1pWtfhVSnRj8udFxW9eTeryGx2mwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('vzDtwf8h7fEZRRmzj4VimopZL0+T1YKif982hzSdorxlLoebj3pj/4FwFipwinhCz87gKYDRvwvWmsU5FJ+0LOhywd+LFkFSopjeArMGhyoDtH823BhMCOUxc0WVSPIfuDwNWbLCemZtEz88kCJhSQdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('dc27fbf1865c82b7c6f63130d25ea736')
+handler = WebhookHandler('1d16422c3b78ca6b26335a808c5258b2')
 
 
 # 監聽所有來自 /callback 的 Post Request
